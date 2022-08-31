27b2ef5421cc592d3dee49d166e533ffd5f4a92f
Euri Tanaka <dev@mugendiary.com>
2022-08-31 16:49:38 -0700

    adding roles

--- frontend/src/places/PlaceDetails.js
+++ frontend/src/places/PlaceDetails.js
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext} from "react";
 import { useHistory, useParams } from "react-router"
 import CommentCard from './CommentCard'
 import NewCommentForm from "./NewCommentForm";
+import { CurrentUser } from '../contexts/CurrentUser';
+
+
 
 function PlaceDetails() {
+
+	const { currentUser } = useContext(CurrentUser)
 
 	const { placeId } = useParams()
 
@@ -102,6 +107,20 @@ function PlaceDetails() {
 		})
 	}
 
+	let placeActions = null
+	if (currentUser?.role === 'admin') {
+		placeActions = (
+			<>
+						<a className="btn btn-warning" onClick={editPlace}>
+							Edit
+						</a>
+						<button type="submit" className="btn btn-danger" onClick={deletePlace}>
+							Delete
+						</button>
+			</>
+		)
+	}
+
 
 	return (
 		<main>
@@ -129,12 +148,7 @@ function PlaceDetails() {
 						Serving {place.cuisines}.
 					</h4>
 					<br />
-					<a className="btn btn-warning" onClick={editPlace}>
-						Edit
-					</a>{` `}
-					<button type="submit" className="btn btn-danger" onClick={deletePlace}>
-						Delete
-					</button>
+					{placeActions}
 				</div>
 			</div>
 			<hr />
